e7ccb7580312eb8aa3cee5fbc245ab91959384c0
reza babakhani <babakhani.reza@gmail.com>
2015-08-24 15:19:36 +0430

    add iworkdirective.js

---
+++ Web/app/js/iworkdirective.js
@@ -0,0 +1,35 @@
+﻿
+iWork.directive('searchNotifier', function (publicSearchDataService) {
+    return {
+        restrict: "EA",
+        link: function ($scope, element, attrs) {
+            if (publicSearchDataService.term && publicSearchDataService.term.length > 0) {
+                element.show();
+            } else {
+                element.hide();
+            }
+        }
+    }
+});
+
+
+iWork.directive('smartInputDirection', function () {
+    return {
+        restrict: "EA",
+        link: function ($scope, element, attrs) {
+            element.keyup(function () {
+                var thiVal = $(this).val().charAt(0)
+                , p = /^[\u0600-\u06FF\s]+$/;
+                if (p.test(thiVal)) {
+                    element.css({
+                        direction: 'rtl'
+                    });
+                } else {
+                    element.css({
+                        direction: 'ltr'
+                    });
+                }
+            });
+        }
+    }
+})
